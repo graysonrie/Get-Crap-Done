c9276c7721afee4142f4a5808c2a3f9576f6bae3
graysonrie <graysonr12@icloud.com>
2026-01-28 08:19:56 -0600

    y

--- src-tauri/src/services/projects_service/mod.rs
+++ src-tauri/src/services/projects_service/mod.rs
@@ -1,12 +1,10 @@
 pub mod service;
 pub mod tauri_exports;
 pub use service::ProjectsService;
-mod image_loader;
+mod components;
 mod models;
-use image_loader::ImageLoaderComponent;
+use components::image_loader::ImageLoaderComponent;
 use models::*;
-mod image_evaluations;
-use image_evaluations::ImageEvaluationsComponent;
+use components::image_evaluations::ImageEvaluationsComponent;
 
 mod requests;
-use requests::*;
